142c6b98daf90d370938a49c9bc0ab17a48fda0e
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-04-23 11:42:16 +0330

    init cleaner commit

--- todolist-app/src/components/Todo.js
+++ todolist-app/src/components/Todo.js
@@ -1,16 +1,19 @@
-import { BsTrash } from "react-icons/bs"
-const Todo = ({ todos , onComplete,onDelete,onEdit}) => {
+import { BsTrash } from "react-icons/bs";
+const Todo = ({ todos, onComplete, onDelete, onEdit }) => {
   return (
     <div className={todos.isCompleted ? "completedTodo" : "todo"}>
       <div>{todos.text}</div>
       <div>
         <button onClick={onEdit}>Edit</button>
-        <button onClick={onComplete}>{todos.isCompleted ? "completed" : "complete"}</button>
-        <button onClick={onDelete}><BsTrash/></button>
+        <button onClick={onComplete}>
+          {todos.isCompleted ? "completed" : "complete"}
+        </button>
+        <button onClick={onDelete}>
+          <BsTrash />
+        </button>
       </div>
     </div>
   );
-
 };
 
 export default Todo;
